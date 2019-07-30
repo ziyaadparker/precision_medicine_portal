aea59c4779578230708a219a49e548ad72954687
Aymenchaalia <aymenbenchaalia1@gmail.com>
2019-07-30 11:30:30 +0100

    template

--- pm_app/views.py
+++ pm_app/views.py
@@ -9,3 +9,24 @@ def how_to_use(request):
 
 def how_to_cite(request):
     return render(request, 'how_to_cite.html')
+
+def About(request):
+    return render(request, 'About.html')
+def Summary(request):
+    return render(request, 'Summary.html')
+def Useful(request):
+    return render(request, 'Useful.html')
+def Online(request):
+    return render(request, 'Online.html')
+def Tools(request):
+    return render(request, 'Tools.html')
+def Contact(request):
+    return render(request, 'Contact.html')
+def Events(request):
+    return render(request, 'Events.html')
+def Announcement(request):
+    return render(request, 'Announcement.html')
+def FeedBack(request):
+    return render(request, 'FeedBack.html')
+def FAQS(request):
+    return render(request, 'FAQS.html')
